74c7941a5aef33931019cf46960ab3e805d3485c
Victor <reivictor.dev@gmail.com>
2024-01-28 11:06:42 -0300

    ... edit CSRF

--- ibbnj_church/settings.py
+++ ibbnj_church/settings.py
@@ -32,6 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/login"
